07d6edfca4311f61f1cb35dde27dbd71888327ae
Kristina <kritoe00165@stud.noroff.no>
2024-10-20 18:50:03 +0200

    organisering

---
+++ Script.js
@@ -0,0 +1,31 @@
+[
+    {
+      "id": "b8b528fc-6c60-41f6-a5a9-9a8b27a9482a",
+      "title": "Rainy Days Akra Jacket",
+      "description": "The Women's Rainy Days Akra jacket is bound to be your new go-to water-repellent rain jacket.",
+      "gender": "Female",
+      "sizes": ["XS", "S", "M", "L", "XL", "XXL"],
+      "baseColor": "Red",
+      "price": 129.99,
+      "discountedPrice": 119.99,
+      "onSale": true,
+      "image": "https://static.cloud.noroff.dev/api/rainy-days/0-akra-jacket.jpg",
+      "tags": ["jacket", "womens"],
+      "favorite": true
+    },
+    {
+      "id": "97e77845-a485-4301-827f-51b673d4230f",
+      "title": "Rainy Days Rocket Jacket",
+      "description": "The unisex Rainy Days Rocket jacket delivers waterproof, breathable protection from head to waist. Perfect for the adventure seekers.",
+      "gender": "Unisex",
+      "sizes": ["S", "M", "L", "XL"],
+      "baseColor": "Yellow",
+      "price": 1990,
+      "discountedPrice": 99.99,
+      "onSale": false,
+      "image": "/Users/kristinatonnessen/Desktop/Rainydays-w-JS/images/mens_jackets/Rocket_jacket_05.jpg",
+      "tags": ["jacket", "womens", "mens", "unisex"],
+      "favorite": true
+    }
+    // ...
+  ]
